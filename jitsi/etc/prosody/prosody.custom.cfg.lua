bccf06d75386bdf3641e82220bb50306b295de2e
emrah <emrah.eryilmaz@nordeck.net>
2022-10-22 14:54:04 +0300

    feat: prosody customization in global config file

---
+++ jitsi/etc/prosody/prosody.custom.cfg.lua
@@ -0,0 +1,13 @@
+-- this is only for documentation, the actual changes are made by the installer
+-- scripts.
+--
+-- Rate limits for incoming connections:
+--
+-- limits = {
+--   c2s = {
+--     rate = "1024kb/s";
+--   };
+--   s2sin = {
+--     rate = "1024kb/s";
+--   };
+-- }
