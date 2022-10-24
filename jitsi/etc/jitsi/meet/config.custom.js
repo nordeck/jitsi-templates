938f25553301270813cf6961d8069e36c87e1675
emrah <emrah.eryilmaz@nordeck.net>
2022-10-24 14:15:04 +0300

    docs: jitsi-meet configs for sip

---
+++ jitsi/etc/jitsi/meet/config.custom.js
@@ -0,0 +1,14 @@
+// This is only for documentation. The actual changes are made by the installer
+// scripts.
+//
+//
+// SIP related changes:
+//
+// - Set disableProfile:
+//
+//   disableProfile: true,
+//
+// - Enable dial-plan search:
+//
+//   peopleSearchQueryTypes: ['conferenceRooms'],
+//   peopleSearchUrl: 'https://jitsi.nordeck.corp/get-dial-plan',
